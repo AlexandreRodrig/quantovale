0eb01323715dea1c568df00c07f58f974929db8f
alexanrodrig@gmail.com <alexanrodrig@gmail.com@cc17b16c-b6db-146b-bd0a-58a3ea05d533>
2015-03-08 16:36:38 +0000

    add condiçao getsEstado !=null

--- quantovale/src/main/java/com/quantovale/controllers/CombosController.java
+++ quantovale/src/main/java/com/quantovale/controllers/CombosController.java
@@ -2,13 +2,10 @@ package com.quantovale.controllers;
 
 import java.util.List;
 
-import javax.management.InvalidAttributeValueException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,12 +22,15 @@ public class CombosController {
 	private CarregaComboService carregaComboService;
 
 	@RequestMapping(value = "/carregaEstado", method = RequestMethod.GET)
-	public String carregarCombo(HttpServletRequest request){ 
+	public String carregarCombo(HttpServletRequest request) {
 		List<Estados> listaEstados = carregaComboService.carregaEstados();
-		Usuario usuario = (Usuario) request.getSession().getAttribute("usuario");
-		if(usuario!=null && !usuario.getEstado().equals("")){
-			List<Cidades> listaCidades = carregaComboService.carregaCidades(Integer.parseInt(usuario.getEstado()));
-			request.setAttribute("listaCidades", listaCidades);			
+		Usuario usuario = (Usuario) request.getSession()
+				.getAttribute("usuario");
+		if (usuario != null && usuario.getEstado() != null
+				&& !usuario.getEstado().equals("")) {
+			List<Cidades> listaCidades = carregaComboService
+					.carregaCidades(Integer.parseInt(usuario.getEstado()));
+			request.setAttribute("listaCidades", listaCidades);
 		}
 		request.getSession().setAttribute("listaEstados", listaEstados);
 		request.setAttribute("listaEstados", listaEstados);
@@ -39,12 +39,14 @@ public class CombosController {
 	}
 
 	@RequestMapping(value = "/carregaCidade", method = RequestMethod.GET)
-	public String carregarComboCidade(HttpServletRequest request,@RequestParam("estado") String estado){ 
+	public String carregarComboCidade(HttpServletRequest request,
+			@RequestParam("estado") String estado) {
 
-		if(!estado.trim().equals("")){
+		if (!estado.trim().equals("")) {
 			int est = Integer.parseInt(estado);
-			List<Cidades> listaCidades = carregaComboService.carregaCidades(est);
-			request.setAttribute("listaCidades", listaCidades);	
+			List<Cidades> listaCidades = carregaComboService
+					.carregaCidades(est);
+			request.setAttribute("listaCidades", listaCidades);
 			request.setAttribute("estadoselecionado", estado);
 		}
 
