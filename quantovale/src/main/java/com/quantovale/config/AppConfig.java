908d1124df0a9cd83cab098a33f2c60efa3a6680
Alexandre <alexanrodrig@gmail.com>
2015-03-21 12:30:56 -0300

    zax

--- quantovale/src/main/java/com/quantovale/config/AppConfig.java
+++ quantovale/src/main/java/com/quantovale/config/AppConfig.java
@@ -5,23 +5,12 @@ package com.quantovale.config;
 
 import java.util.Properties;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.omg.CORBA.Environment;
 
 /**
- * @author Siva
+ * @author
+ * 
+ * 
  *
  */
 @Configuration
